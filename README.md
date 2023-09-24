9565cd84255b30dd6a7154601ed58c1a4e29018a
sjain882 <43217178+sjain882@users.noreply.github.com>
2023-09-24 16:14:24 +0100

    Initial upload

---
+++ README.md
@@ -0,0 +1,2 @@
+# Browser Tweaks
+Miscellanous userscripts, uBlock Origin filters/lists and more. Created/used by me to enhance my web browsing experience.
---
+++ WebpageMarginsKeyboard.user.js
@@ -0,0 +1,37 @@
+// ==UserScript==
+// @name         Adjust horizontal webpage margins with keyboard
+// @namespace    https://www.github.com/sjain882
+// @version      0.1.0
+// @description  Keyboard shortcuts to move the entire webpage left/right independently of the window dimensions - useful for oddly angled monitors, etc.
+// @author       sjain882
+// @match        *://*/*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant           GM_addStyle
+// @homepageURL     https://github.com/sjain882/Userscripts
+// @supportURL      https://github.com/sjain882/Userscripts/issues
+// @updateURL       https://raw.githubusercontent.com/sjain882/Userscripts/main/WebpageMarginsKeyboard.user.js
+// @downloadURL     https://raw.githubusercontent.com/sjain882/Userscripts/main/WebpageMarginsKeyboard.user.js
+// ==/UserScript==
+
+var horizontalMargin = 0;
+var adjustStep = 50;
+
+window.addEventListener('keydown', function(e) {
+
+    // Shift page contents right
+    if (e.shiftKey && e.key == 'PageDown') {
+        horizontalMargin = horizontalMargin + adjustStep;
+        GM_addStyle("html{margin-left:" + horizontalMargin + "px}");
+
+    // Shift page contents left
+    } else if (e.shiftKey && e.key == 'Delete') {
+        horizontalMargin = horizontalMargin - adjustStep;
+        GM_addStyle("html{margin-left:" + horizontalMargin + "px}");
+
+    // Reset
+    } else if (e.shiftKey && e.key == 'End') {
+        horizontalMargin = 0;
+        GM_addStyle("html{margin-left: 0px}");
+    }
+
+}, true);
