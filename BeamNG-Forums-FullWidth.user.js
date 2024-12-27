df4a91c66ffd7ef2548c06ab451e992a7e3bdb26
sjain882 <43217178+sjain882@users.noreply.github.com>
2024-12-27 09:25:04 +0000

    bng

--- BeamNG-Forums-FullWidth.user.js
+++ BeamNG-Forums-FullWidth.user.js
@@ -14,11 +14,6 @@
 // @updateURL https://raw.githubusercontent.com/sjain882/Userscripts/main/BeamNG-Forums-FullWidth.user.js
 // ==/UserScript==
 
-// Toggling devtools, fullscreen, or changing the window dimensions (width or height) seems to break this script.
-// I suspect this is because .mainContent's width is set in the js (as it's applied directly to the element in HTML)
-// and every time the page dimensions change, this width value is re-applied by the js.
-// I can't fix this because the js a browser recieves is obfustucated and minified.
-
 // To set a custom width, replace 97 with your desired page-width percentage.
 GM_addStyle(".pageWidth { max-width: 97%; }");
 
@@ -27,14 +22,28 @@ GM_addStyle(".pageWidth { max-width: 97%; }");
  * by expanding the sidebar by 100px & shrinking the main content by 100px.
  * No, I didn't make this margin bodge, it was written like that on the original pages. */
 
+function adjustPageLayout() {
+
 // for /forums:
-GM_addStyle(".sidebar { width: 350px; !important }");
-GM_addStyle(".mainContent { margin-left: 362px; !important }");
+GM_addStyle(".sidebar { width: 350px !important; }");
+GM_addStyle(".mainContent { margin-left: 362px !important; }");
+GM_addStyle(".mainContent. { margin-left: 362px !important; }");
 
 // for /resources:
-GM_addStyle(".resourceListSidebar { width: 320px; !important }");
-GM_addStyle(".resourceListMain { margin-left: 330px; !important }");
+GM_addStyle(".resourceListSidebar { width: 320px !important; }");
+GM_addStyle(".resourceListMain { margin-left: 330px !important; }");
 
 // Fix announcement panels.
 // If this is broken, use CTRL + F5
-GM_addStyle("li.panel:nth-child() { width: 1234px; !important }");
+GM_addStyle("li.panel:nth-child() { width: 1234px !important; }");
+
+}
+
+
+/* .mainContent's width is set in the BeamNG forums js as the page is loaded.
+ * To override this, we must adjust the layout *after* the page has fully loaded. */
+window.addEventListener('load', function() {
+    adjustPageLayout();
+}, false);
+
+
